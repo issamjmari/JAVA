8ed9aee14e50eb739cff9344a980610f63628b3a
Issam Jmari <ijmari@c3r5p10.1337.ma>
2024-10-16 16:32:33 +0100

    ex02 in java02

---
+++ JAVA02/ex02/Program.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.DirectoryStream;
+import java.io.IOException;
+
+class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        String directory = args[0].split("=")[1];
+        while(true) {
+            try {
+                input = scanner.nextLine();
+                if (input.equals("ls")) {
+                    try {
+                        Path dirPath = Paths.get(directory);
+                        if (Files.isDirectory(path)) {
+                            try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+                                for (Path entry : stream) {
+                                    System.out.println(entry.getFileName());
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } 
+                        else {
+                            System.out.println("The path is not a directory.");
+                        }
+                    }
+                    catch(InvalidPathException e) {
+                        System.err.println("Error: Invalid path - " + e.getMessage());
+                    }
+                }
+            }
+            catch (Exception e) {
+
+            }
+        }
+    }
+}
